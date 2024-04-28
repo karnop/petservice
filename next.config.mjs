51d200c7fa41e6f0536ec8c95683a111e62341a0
karnop <communicate.manav@gmail.com>
2024-04-28 16:04:33 +0530

    disabled eslint rules for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        // PROJECT_URL: "http://localhost:3000/",
+        PROJECT_URL: "https://petserviceco.vercel.app/",
+    },
+
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
